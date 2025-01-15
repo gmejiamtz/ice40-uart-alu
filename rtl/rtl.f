3206be1d4d5ef8823d766ff21ee6d3c7b9862db1
Mitchell Tansey <143773715+MitchTansey@users.noreply.github.com>
2025-01-15 22:10:33 +0000

    Added dependencies and created top mod

--- rtl/rtl.f
+++ rtl/rtl.f
@@ -2,6 +2,10 @@
 -I${BASEJUMP_STL_DIR}/bsg_misc
 ${BASEJUMP_STL_DIR}/bsg_misc/bsg_counter_up_down.sv
 
+-I${ALEXFORENCICH_UART_DIR}/rtl
+${ALEXFORENCICH_UART_DIR}/rtl/uart_rx.v
+${ALEXFORENCICH_UART_DIR}/rtl/uart_tx.v
+
 rtl/config_pkg.sv
 
 rtl/top.sv
