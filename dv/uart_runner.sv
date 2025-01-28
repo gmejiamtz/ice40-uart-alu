be2588adc0eb2c617a4ec8ec1805dd4ad6df0083
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2025-01-28 02:19:32 +0000

    Checkpoint for part1.2, waveform done echoing behavior seen

--- dv/uart_runner.sv
+++ dv/uart_runner.sv
@@ -5,15 +5,17 @@ localparam DATA_WIDTH_P = 8;
 
 logic clk_i;
 logic rst;
-logic tx_i;
-logic data_o;
+logic tx_i,tx_o;
+logic [DATA_WIDTH_P-1:0] data_i,data_o;
+logic t_valid_i;
 
 //uart_device signals
+/*
 logic [DATA_WIDTH_P-1:0] uart_device_data_i;
 logic uart_device_tvalid_i,uart_device_tready_o,
     uart_device_txd_o,uart_device_busy_o,
     uart_device_prescale_i;
-
+*/
 localparam realtime ClockPeriod = 5ms;
 
 initial begin
@@ -26,8 +28,13 @@ end
 
 top #() top_uut (.clk(clk_i),
     .rst(rst),
-    .rx_i(uart_device_txd_o),
-    .tx_o(data_o));
+    .data_i(data_i),
+    .t_valid_i(t_valid_i),
+    .tx_o(tx_o),
+    .data_o(data_o)
+);
+
+/*
 
 uart_tx #(.DATA_WIDTH(DATA_WIDTH_P)) uart_device(
     .clk(clk_i),
@@ -43,17 +50,32 @@ uart_tx #(.DATA_WIDTH(DATA_WIDTH_P)) uart_device(
 always @(posedge uart_device_busy_o) $info("Uart Transmitter busy");
 always @(negedge uart_device_busy_o) $info("Uart Transmitter not busy!");
 
+*/
+
 task automatic reset;
     rst <= 1;
-    @(posedge clk_i);
+    data_i <= '0;
+    t_valid_i <= '0;
+    repeat (5) begin
+        @(posedge clk_i);
+    end
     rst <= 0;
+    repeat (5) begin
+        @(posedge clk_i);
+    end
 endtask
 
 task automatic send_data (input [DATA_WIDTH_P-1:0] data_in);
-    uart_device_data_i <= data_in;
-    uart_device_tvalid_i <= 1'b1;
-    while (uart_device_busy_o) @(posedge clk_i);
-    uart_device_tvalid_i <= 1'b0;
+    data_i <= data_in;
+    t_valid_i <= 1'b1;
+    $info("Sending %h\n",data_in);
+    @(posedge clk_i);
+endtask
+
+task automatic wait_cycle(integer n);
+    repeat (n) begin
+        @(posedge clk_i);
+    end
 endtask
 
 endmodule
--- rtl/top.sv
+++ rtl/top.sv
@@ -1,34 +1,40 @@
 module top (
     input clk,
     input rst,
-    input rx_i,
-    output tx_o
+    input [7:0] data_i,
+    input t_valid_i,
+    output tx_o,
+    output [7:0] data_o
 );
 
-wire [7:0] data_o;
+logic t_ready_o,r_ready_i,r_valid_o;
+
+wire [7:0] actual_data_in;
+
+assign actual_data_in = data_i & {8{t_valid_i}} | data_o & {8{r_valid_o}};
 
 uart_rx #(.DATA_WIDTH(8)) uart_rx_inst (
     .clk(clk),
     .rst(rst),
     .m_axis_tdata(data_o),
-    .m_axis_tvalid(),
-    .m_axis_tready(),
-    .rxd(rx_i),
+    .m_axis_tvalid(r_valid_o),
+    .m_axis_tready(t_ready_o),
+    .rxd(tx_o),
     .busy(),
     .overrun_error(),
     .frame_error(),
-    .prescale()
+    .prescale(1)
 );
 
 uart_tx #(.DATA_WIDTH(8)) uart_tx_inst (
     .clk(clk),
     .rst(rst),
-    .s_axis_tdata(data_o),
-    .s_axis_tvalid(),
-    .s_axis_tready(),
+    .s_axis_tdata(actual_data_in),
+    .s_axis_tvalid(t_valid_i | r_valid_o),
+    .s_axis_tready(t_ready_o),
     .txd(tx_o),
     .busy(),
-    .prescale()
+    .prescale(1)
 );
 
 endmodule
