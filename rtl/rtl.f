a1b9f51f1b8c681941cbca72e29a6e99295175da
Gary Mejia <63254578+gmejiamtz@users.noreply.github.com>
2025-01-30 06:02:57 +0000

    Got GLS working for no tech

--- rtl/rtl.f
+++ rtl/rtl.f
@@ -5,8 +5,8 @@ ${BASEJUMP_STL_DIR}/bsg_misc/bsg_counter_up_down.sv
 -I${ALEXFORENCICH_UART_DIR}/rtl
 ${ALEXFORENCICH_UART_DIR}/rtl/uart_rx.v
 ${ALEXFORENCICH_UART_DIR}/rtl/uart_tx.v
+${ALEXFORENCICH_UART_DIR}/rtl/uart.v
 
 rtl/config_pkg.sv
 
 rtl/top.sv
-
--- rtl/top.sv
+++ rtl/top.sv
@@ -8,12 +8,6 @@ module top (
 logic [7:0] rx_data_out;
 logic rx_valid_out;
 
-//logic t_ready_o,r_valid_o;
-
-//wire [7:0] actual_data_in;
-
-//assign actual_data_in = data_i & {8{t_valid_i}} | data_o & {8{r_valid_o}};
-
 uart_rx #(.DATA_WIDTH(8)) uart_rx_inst (
     .clk(clk),
     .rst(rst),
@@ -37,31 +31,5 @@ uart_tx #(.DATA_WIDTH(8)) uart_tx_inst (
     .busy(),
     .prescale(1)
 );
-
-
-// uart_rx #(.DATA_WIDTH(8)) uart_rx_inst (
-//     .clk(clk),
-//     .rst(rst),
-//     .m_axis_tdata(data_o),
-//     .m_axis_tvalid(r_valid_o),
-//     .m_axis_tready(t_ready_o),
-//     .rxd(tx_o),
-//     .busy(),
-//     .overrun_error(),
-//     .frame_error(),
-//     .prescale(1)
-// );
-
-// uart_tx #(.DATA_WIDTH(8)) uart_tx_inst (
-//     .clk(clk),
-//     .rst(rst),
-//     .s_axis_tdata(actual_data_in),
-//     .s_axis_tvalid(t_valid_i | r_valid_o),
-//     .s_axis_tready(t_ready_o),
-//     .txd(tx_o),
-//     .busy(),
-//     .prescale(1)
-// );
-
 
 endmodule
